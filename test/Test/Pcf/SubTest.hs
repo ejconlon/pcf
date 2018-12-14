b00dea10fcb7170b1b8a94103cf49a06b08d23b0
Eric Conlon <econlon@lyft.com>
2018-12-14 12:42:23 -0800

    subtest

--- test/Test/Pcf/SubTest.hs
+++ test/Test/Pcf/SubTest.hs
@@ -22,11 +22,11 @@ type Exp n a = Scope (ExpF n) a
 var :: a -> Exp n a
 var = pure
 
-lam :: Eq a => n -> a -> Exp n a -> Exp n a
-lam n a = wrapScope . Lam (Name n ()) . abstract1 a
+lam' :: Eq a => n -> a -> Exp n a -> Exp n a
+lam' n a = wrapScope . Lam (Name n ()) . abstract1 a
 
-lam' :: Eq n => n -> Exp n n -> Exp n n
-lam' n = lam n n
+lam :: Eq n => n -> Exp n n -> Exp n n
+lam n = lam' n n
 
 app :: Exp n a -> Exp n a -> Exp n a
 app l r = wrapScope (App l r)
@@ -42,13 +42,25 @@ suc = wrapScope  . Suc
 
 -- Example terms
 
-eid, econst, efree, efree2, evar :: Exp Char Char
-eid = lam' 'x' (var 'x')
-econst = lam' 'a' (lam' 'b' (var 'a'))
-efree = lam' 'y' (var 'z')
-efree2 = lam' 'c' (lam' 'd' (var 'e'))
+eid, econst, efree, efree2, evar, ezero :: Exp Char Char
+eid = lam 'x' (var 'x')
+econst = lam 'a' (lam 'b' (var 'a'))
+efree = lam 'y' (var 'z')
+efree2 = lam 'c' (lam 'd' (var 'e'))
 evar = var 'w'
+evar2 = var 'u'
+ezero = zero
 
-test_something :: TestTree
-test_something = testCase "something" $ do
-    1 + 1 @?= 2
+test_varsub :: TestTree
+test_varsub = testCase "varsub" $ do
+    (ezero >>= (const evar2)) @?= ezero
+    (evar >>= (const evar2)) @?= evar2
+    (efree >>= (const evar2)) @?= lam 'y' evar2
+    (efree2 >>= (const evar2)) @?= lam 'c' (lam 'd' evar2)
+
+test_idsub :: TestTree
+test_idsub = testCase "idsub" $ do
+    (ezero >>= (const eid)) @?= ezero
+    (evar >>= (const eid)) @?= eid
+    (efree >>= (const eid)) @?= lam 'y' eid
+    -- (efree2 >>= (const eid)) @?= lam 'c' (lam 'd' eid)
