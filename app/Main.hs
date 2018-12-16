91889c768e3da3716bc721d1cff92cabf86413d0
Eric Conlon <econlon@lyft.com>
2018-12-16 15:58:44 -0800

    v2 repl

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import qualified Pcf.V1.Repl as PR
+import qualified Pcf.V2.Repl as PR
 
 main :: IO ()
 main = PR.main
---
+++ src/Pcf/V2/Repl.hs
@@ -0,0 +1,30 @@
+module Pcf.V2.Repl where
+
+import           Control.Monad.IO.Class     (liftIO)
+import           Control.Monad.State.Strict (get, put)
+import           Data.Map.Strict            (Map)
+import qualified Data.Map.Strict            as M
+import           Pcf.Core.Cli               (Cli, Command, ReplDirective(..), outputPartsLn, outputPretty, outputStrLn)
+import           Pcf.Core.NiceRepl          (OptionCommands, ReplDef (..), runRepl, throwCommandError)
+
+type Repl = Cli ()
+type ReplCommand = Command ()
+type ReplOptionCommands = OptionCommands ()
+
+execCommand :: ReplCommand
+execCommand input = do
+    outputStrLn "TODO: exec"
+    pure ReplContinue
+
+evalCommand :: ReplCommand
+evalCommand input = do
+    outputStrLn "TODO: eval"
+    pure ReplContinue
+
+additionalOptions :: ReplOptionCommands
+additionalOptions = M.fromList
+    [ ("eval", ("evaluate an expression", evalCommand))
+    ]
+
+main :: IO ()
+main = runRepl (ReplDef "Welcome to the PCF V2 Repl." () additionalOptions execCommand)
