c04ae3699995cd6bda84dbcb7dc56ed564cd15f4
Eric Conlon <econlon@lyft.com>
2018-12-18 20:16:01 -0800

    info

--- src/Pcf/V2/Types.hs
+++ src/Pcf/V2/Types.hs
@@ -8,23 +8,23 @@ import Pcf.Core.Sub
 -- SExp
 
 data SExp i a =
-      SAtom !i !a
-    | SList !i !(Vector (SExp i a))
+      SAtom i a
+    | SList i (Vector (SExp i a))
     deriving (Generic, Eq, Show, Functor, Foldable, Traversable)
 
 -- Ty
 
 data Ty =
-      Arr !Ty !Ty
+      Arr Ty Ty
     | Nat deriving (Generic, Eq, Show)
 
 -- Exp
 
 data ExpF a =
-      App !a !a
-    | Ifz !a !a !a
+      App a a
+    | Ifz a a a
     | Zero
-    | Suc !a
+    | Suc a
     deriving (Generic, Eq, Show, Functor, Foldable, Traversable)
 
 data ExpN n = ExpN { expName :: !(NameOnly n), expTy :: !Ty } deriving (Generic, Eq, Show)
@@ -40,18 +40,18 @@ type Exp n a = Scope (ExpN n) ExpF a
 -- Stmt
 
 data Stmt n a =
-      Decl !n !Ty
-    | Defn !n !(Exp n a)
+      Decl n Ty
+    | Defn n (Exp n a)
     deriving (Generic, Eq, Show, Functor, Foldable, Traversable)
 
 -- ExpC
 
 data ExpCF a =
-      AppC !a !a
-    | IfzC !a !a !a
-    | SucC !a
+      AppC a a
+    | IfzC a a a
+    | SucC a
     | ZeroC
-    | ClosC !(Vector a) !a
+    | ClosC (Vector a) a
     deriving (Generic, Eq, Show, Functor, Foldable, Traversable)
 
 type ExpC n a = Scope (ExpN n) ExpCF a
